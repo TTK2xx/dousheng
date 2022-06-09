1df2d56fe0a969f8d09eb79d4e47975cf14d2b5a
hcl <1379130749@qq.com>
2022-06-09 20:48:38 +0800

    同步代码

--- controller/PublishController.go
+++ controller/PublishController.go
@@ -3,16 +3,18 @@ package controller
 import (
 	"dousheng/common"
 	"github.com/gin-gonic/gin"
+	idworker "github.com/gitstliu/go-id-worker"
+	"mime/multipart"
 	"net/http"
 )
 
-type PublishActionRequest struct {
+type PublishRequest struct {
 	Token string `form:"token" json:"token" binding:"required"`
 	Data  byte   `form:"data" json:"data" binding:"required"`
 	Title string `form:"title" json:"title" binding:"required"`
 }
 
-type PublishActionResponse struct {
+type PublishResponse struct {
 	common.Response
 }
 
@@ -26,32 +28,61 @@ type PublishListResponse struct {
 	VideoList []Video `json:"video_list"`
 }
 
+var videoFileExt = []string{"mp4", "flv"} //此处可根据需要添加格式
+var idGen *idworker.IdWorker
+
+func init() {
+	idGen = &idworker.IdWorker{}
+	idGen.InitIdWorker(1, 1)
+}
+
+func IsVideoAllowed(suffix string) bool {
+	for _, fileExt := range videoFileExt {
+		if suffix == fileExt {
+			return true
+		}
+	}
+	return false
+}
+
+func UploadVideo(file *multipart.FileHeader) (err error) {
+	//filename := file.Filename                      //获取文件名
+	//indexOfDot := strings.LastIndex(filename, ".") //获取文件后缀名前的.的位置
+	//if indexOfDot < 0 {
+	//	return errors.New("没有获取到文件的后缀名")
+	//}
+	//suffix := filename[indexOfDot+1 : len(filename)] //获取后缀名
+	//suffix = strings.ToLower(suffix)                 //后缀名统一小写处理
+	//if !IsVideoAllowed(suffix) {
+	//	return errors.New("上传的文件不符合视频的格式")
+	//}
+	//fmt.Println("刚才上传的文件后缀名：" + suffix)
+	//id, err := idGen.NextId()
+	//filename = strconv.FormatInt(id, 10)
+	//filename = filename + "." + suffix
+	//data, err := file.Open()
+	//folderName := "video"
+	//key := folderName + "/" + filename
+	return err
+}
 func Publish(c *gin.Context) {
-	var request PublishActionRequest
-	if err := c.ShouldBind(&request); err != nil {
+	file, err := c.FormFile("data")
+	err = UploadVideo(file)
+	if err != nil {
 		c.JSON(http.StatusOK, common.Response{
 			StatusCode: common.ParamInvalid,
-			StatusMsg:  "Parameter parsing error",
+			StatusMsg:  "Publish Parameter parsing error",
 		})
 		return
 	}
-	//token := request.Token
-	//strs := strings.Split(token, ":")
-	//username := strs[0]
-	//user, err := service.GetUserByUsername(username)
-	//
-	//if err != nil {
+	//var request PublishRequest
+	//if err := c.ShouldBind(&request); err != nil {
 	//	c.JSON(http.StatusOK, common.Response{
-	//		StatusCode: 1,
-	//		StatusMsg:  err.Error(),
+	//		StatusCode: common.ParamInvalid,
+	//		StatusMsg:  "Publish Parameter parsing error",
 	//	})
 	//	return
 	//}
-	//
-	//c.JSON(http.StatusOK, common.Response{
-	//	StatusCode: 0,
-	//	StatusMsg:  " uploaded successfully",
-	//})
 }
 
 func PublishList(c *gin.Context) {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.8.0
+	github.com/gitstliu/go-id-worker v0.0.0-20190725025543-5a5fe074e612
 	gorm.io/driver/mysql v1.3.4
 	gorm.io/gorm v1.23.5
 )
